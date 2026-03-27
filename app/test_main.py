87c8e39e3ce3f001f31bc1ab90c8281d48f2f382
martoff <martoff@ukr.net>
2026-03-27 22:15:35 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -5,7 +5,7 @@ import app.main as main
 def test_should_buy_when_prediction_more_than_5_percent(
     monkeypatch: pytest.MonkeyPatch
 ) -> None:
-    monkeypatch.setattr(main, "get_exchange_rate_prediction", lambda: 106)
+    monkeypatch.setattr(main, "get_exchange_rate_prediction", lambda rate: 106)
 
     assert main.cryptocurrency_action(100) == "Buy more cryptocurrency"
 
@@ -13,7 +13,7 @@ def test_should_buy_when_prediction_more_than_5_percent(
 def test_should_sell_when_prediction_less_than_5_percent(
     monkeypatch: pytest.MonkeyPatch
 ) -> None:
-    monkeypatch.setattr(main, "get_exchange_rate_prediction", lambda: 94)
+    monkeypatch.setattr(main, "get_exchange_rate_prediction", lambda rate: 94)
 
     assert main.cryptocurrency_action(100) == "Sell all your cryptocurrency"
 
@@ -21,7 +21,7 @@ def test_should_sell_when_prediction_less_than_5_percent(
 def test_should_do_nothing_when_change_within_5_percent(
     monkeypatch: pytest.MonkeyPatch
 ) -> None:
-    monkeypatch.setattr(main, "get_exchange_rate_prediction", lambda: 104)
+    monkeypatch.setattr(main, "get_exchange_rate_prediction", lambda rate: 104)
 
     assert main.cryptocurrency_action(100) == "Do nothing"
 
@@ -29,6 +29,6 @@ def test_should_do_nothing_when_change_within_5_percent(
 def test_should_do_nothing_when_change_exactly_5_percent(
     monkeypatch: pytest.MonkeyPatch
 ) -> None:
-    monkeypatch.setattr(main, "get_exchange_rate_prediction", lambda: 105)
+    monkeypatch.setattr(main, "get_exchange_rate_prediction", lambda rate: 105)
 
     assert main.cryptocurrency_action(100) == "Do nothing"
