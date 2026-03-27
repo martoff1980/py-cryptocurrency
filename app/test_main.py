65c20f88eb232ff8f48a798d3caba14876d0d244
martoff <martoff@ukr.net>
2026-03-27 22:09:12 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,34 @@
-# write your code here
+import pytest
+import app.main as main
+
+
+def test_should_buy_when_prediction_more_than_5_percent(
+    monkeypatch: pytest.MonkeyPatch
+) -> None:
+    monkeypatch.setattr(main, "get_exchange_rate_prediction", lambda: 106)
+
+    assert main.cryptocurrency_action(100) == "Buy more cryptocurrency"
+
+
+def test_should_sell_when_prediction_less_than_5_percent(
+    monkeypatch: pytest.MonkeyPatch
+) -> None:
+    monkeypatch.setattr(main, "get_exchange_rate_prediction", lambda: 94)
+
+    assert main.cryptocurrency_action(100) == "Sell all your cryptocurrency"
+
+
+def test_should_do_nothing_when_change_within_5_percent(
+    monkeypatch: pytest.MonkeyPatch
+) -> None:
+    monkeypatch.setattr(main, "get_exchange_rate_prediction", lambda: 104)
+
+    assert main.cryptocurrency_action(100) == "Do nothing"
+
+
+def test_should_do_nothing_when_change_exactly_5_percent(
+    monkeypatch: pytest.MonkeyPatch
+) -> None:
+    monkeypatch.setattr(main, "get_exchange_rate_prediction", lambda: 105)
+
+    assert main.cryptocurrency_action(100) == "Do nothing"
